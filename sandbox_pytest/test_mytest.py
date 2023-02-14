a2c972e7b9eda42225c77a1169adb878ce41393a
Avinash Goen <agoen97@gmail.com>
2023-02-14 09:52:18 +0530

    Test using multiple fixtures

--- sandbox_pytest/test_mytest.py
+++ sandbox_pytest/test_mytest.py
@@ -2,13 +2,17 @@ from mytest import square
 import pytest
 
 @pytest.fixture
-def input_values():
+def square_input():
     return 2
+
+@pytest.fixture
+def square_output():
+    return 4
 
 def test_square_gives_correct_value():
     out = square(2)
     assert out == 4
 
-def test_square_gices_correct_value(input_values):
-    out = square(input_values)
-    assert out == 4
+def test_square_gices_correct_value(square_input, square_output):
+    out = square(square_input)
+    assert out == square_output
