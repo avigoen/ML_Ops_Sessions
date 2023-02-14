b2218f288be0d6709b4f4df987c870b393fda163
Avinash Goen <agoen97@gmail.com>
2023-02-14 09:46:45 +0530

    Test using fixtures

--- sandbox_pytest/test_mytest.py
+++ sandbox_pytest/test_mytest.py
@@ -1,6 +1,14 @@
 from mytest import square
 import pytest
 
+@pytest.fixture
+def input_values():
+    return 2
+
 def test_square_gives_correct_value():
     out = square(2)
+    assert out == 4
+
+def test_square_gices_correct_value(input_values):
+    out = square(input_values)
     assert out == 4
